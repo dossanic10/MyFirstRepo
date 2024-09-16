3a91647d1162eb87246b0e037c56f250be046ae4
Nicholas Dos Santos <dossanic@sheridancollege.ca>
2024-09-16 16:41:23 -0400

    initial commit - added comment

---
+++ src/demomondayevening/DemoMondayEvening.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demomondayevening;
+
+/**
+ *
+ * @author nickd
+ */
+public class DemoMondayEvening {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello");
+        //added comment
+        // TODO code application logic here
+    }
+    
+}
