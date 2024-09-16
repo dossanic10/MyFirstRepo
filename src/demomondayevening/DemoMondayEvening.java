1d052efd9793bc91680070c1e67ae04bea25bb26
dossanic10 <dossanic@sheridancollege.ca>
2024-09-16 17:12:54 -0400

    Update DemoMondayEvening.java
    fetch and merge

--- src/demomondayevening/DemoMondayEvening.java
+++ src/demomondayevening/DemoMondayEvening.java
@@ -14,6 +14,7 @@ public class DemoMondayEvening {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //fetch and merge
         //making a change on remote
         System.out.println("Hello");
         //added comment
