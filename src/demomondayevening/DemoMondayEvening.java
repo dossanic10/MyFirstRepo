3cf2064a09f8dc43acfa3a0cf45e6cc4cdbc048e
dossanic10 <dossanic@sheridancollege.ca>
2024-09-16 17:10:46 -0400

    Update DemoMondayEvening.java
    remote change

--- src/demomondayevening/DemoMondayEvening.java
+++ src/demomondayevening/DemoMondayEvening.java
@@ -14,7 +14,7 @@ public class DemoMondayEvening {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //making a change on remote
         System.out.println("Hello");
         //added comment
         //second change
